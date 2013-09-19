5d3ee8a52faba0371b57b06fcc63b7cd04bd63bb
Carl Herold <carl@livestreamstartup.com>
2013-09-19 22:23:24 +0000

    Added files at end of session

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 //	Controller
-echo 'testing 3';
+
+	if ($_SERVER['REQUEST_URI'] != '/') {
+		preg_match('!name/([a-z]+)!imsx', $_SERVER['REQUEST_URI'], $pmatches);
+
+		$content = file_get_contents("../templates/index.html");
+		$content = str_replace('{text}', 'Hello, '.$pmatches[1], $content);
+		echo $content;
+
+		// Homepage!
+	} else {
+		echo "You are not on the homepage!";
+		// Not Homepage
+
+	}
+
+	
+
