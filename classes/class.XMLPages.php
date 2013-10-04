822d809ca92425635d139182c5e7abcbeff5934a
Carl Herold <carl@livestreamstartup.com>
2013-10-04 22:32:14 +0000

    Working on dynamic xml like handling

---
+++ classes/class.XMLPages.php
@@ -0,0 +1,70 @@
+<?php
+	class TXMLPages {
+		function __construct() {
+			$file = file("../xmlpages/homepage.xml");
+
+			foreach ($file as $key=>$value) {
+				if (preg_match('!^<(.*?)>!imsx', $value, $pmatches)) {
+					$value = trim($value);
+
+					$parentTag  = 0;
+					$currentTag = $pmatches[1];
+
+					echo "Current Tag: ".$currentTag."<br />\n";
+
+
+					$this->divContent[] 	= $divContent;
+					$this->contentTags[] 	= $currentTag;
+				}
+				if (preg_match('!^([\t]+)!imsx', $value, $pmatches)) {
+					// $pmatches[1] contains all tabs at start of the line, therefore
+					// strlen($pmatches[1]) contains the total number of tabs.
+					$numTabs = strlen($pmatches[1]);
+
+
+					if ($numTabs == 1) {
+						$parentTag = $currentTag;
+					}
+
+					if (preg_match('!<(.*?)>!imsx', $value, $qmatches)) {
+						$childrenTags[ $parentTag ][] = $qmatches[1];
+					}
+
+					echo "Current Tag: ".$currentTag."\n<br />";
+				}
+
+				// Immediately after the line which matches a given tag, check
+				// to see if the closing tag is reached.
+				if (preg_match('!</'.$currentTag.'>')) {
+					echo "Reached end of this tag!";
+				}
+			}
+
+			print_r($this->divContent);
+
+			print_r($childrenTags);
+
+			$divContent = '';
+			foreach($childrenTags as $key=>$value) {
+				if (is_array($value)) {
+
+					$divContent .= "<div class=\"".$key."\">\n";
+
+					foreach($value as $akey=>$avalue) {
+						$divContent .= "\t<div class=\"".$avalue."\">{".$avalue."}</div>\n";
+					}
+
+					$divContent .= "</div>";
+				}
+			}
+
+			echo "\n\n\n";
+			echo $divContent;
+			echo "\n\n\n";
+
+		}
+
+		function getContentTags() {
+			return $this->contentTags;
+		}
+	}
--- classes/pages/class.homepage.php
+++ classes/pages/class.homepage.php
@@ -3,12 +3,22 @@
 	require_once("../classes/class.GeneralPageClass.php");
 	require_once("../classes/class.Database.php");
 
+	require_once("../classes/class.XMLPages.php");
+
 	class TPageClass extends TGeneralPageClass {
 		function init() {
+			// Makes it possible to parse xmlpages/files.xml
+			$this->XMLPages = new TXMLPages();
+
+			$contentTags = $this->XMLPages->getContentTags();
+
 			$this->createContent();
 
-			$this->assignPlaceholder('copyright');
-			$this->assignPlaceholder('navbar');
+			$this->assignPlaceholder('homepage_content');
+
+			foreach ($contentTags as $key=>$value) {
+				$this->assignPlaceholder($value);
+			}
 
 			$this->showContent();
 
